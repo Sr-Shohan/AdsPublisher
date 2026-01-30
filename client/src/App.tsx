7a8d98fccf2280de99394c6f296498544f17bd1a
SHOHAN <shohan@eskimi.com>
2026-01-30 09:51:25 +0600

    Fix router base for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import NotFound from "@/pages/not-found";
 import Home from "@/pages/Home";
 
 function Router() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    <Router base={import.meta.env.BASE_URL}>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
